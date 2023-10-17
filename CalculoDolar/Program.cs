f7acebe963ae9a49151b9fb9a6dcff345dee61e5
FireDragonSrc <firedragonsrc@gmail.com>
2023-10-17 11:02:57 -0300

    commit inicial

---
+++ CalculoDolar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoDolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float cotacaoDolar = 4.99f;
+            Console.WriteLine("Valor:");
+            float reais = int.Parse(Console.ReadLine());
+            Console.WriteLine(reais / cotacaoDolar);
+            Console.ReadLine();
+        }
+    }
+}
